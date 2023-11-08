a0dbffeb68c60ae83d051869c33d54ccf75e2a0e
aussiearef <arefkr@gmail.com>
2023-11-08 11:31:53 +1100

    Added Logging

--- java-app/src/main/java/app.java
+++ java-app/src/main/java/app.java
@@ -1,7 +1,25 @@
 import static spark.Spark.*;
 
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 public class app {
     public static void main(String[] args) {
+
+        Logger logger = Logger.getLogger(app.class.getName());
+        
+        try {
+            // Create a file handler to log to "log.txt"
+            FileHandler fileHandler = new FileHandler("log.txt");
+            SimpleFormatter formatter = new SimpleFormatter();
+            fileHandler.setFormatter(formatter);
+
+            // Add the file handler to the logger
+            logger.addHandler(fileHandler);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         System.out.println("Please navigate to http://localhost:4567/hello");
         // Set up a route that responds to HTTP GET requests at the "/hello" endpoint
@@ -11,6 +29,9 @@ public class app {
 
             // Create a JSON response
             String jsonResponse = "{\"message\": \"Hello, World!\"}";
+
+            // Log a warning message
+            logger.warning("Hello World API Called.");
 
             return jsonResponse;
         });
--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 # Run this code with:
-# NEW_RELIC_CONFIG_FILE=./newrelic.ini newrelic-admin run-program uvicorn app:app --host 0.0.0.0 --port 8000
+# NEW_RELIC_CONFIG_FILE=./newrelic.ini newrelic-admin run-program uvicorn main:app --host 0.0.0.0 --port 8000
 
 
 from fastapi import FastAPI
+import logging
 
 # Create an instance of the FastAPI app
 app = FastAPI()
+logging.basicConfig(filename='log.txt', level=logging.WARNING, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Define a route for the GET request
 @app.get("/")
 async def get_hello_world():
+    logging.warn("Hello World API called.")
     return {"message": "Hello World"}
 
 if __name__ == "__main__":
     import uvicorn
 
-    # Run the FastAPI app using Uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+    logging.shutdown()
